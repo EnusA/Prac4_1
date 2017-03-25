363064021867958f61067559d62e159c3da1d182
EnusA <enusabdullah@gmail.com>
2017-03-25 22:52:20 +0200

    Update App.java

--- src/main/java/com/EnusA/Prac4_1/App.java
+++ src/main/java/com/EnusA/Prac4_1/App.java
@@ -7,7 +7,14 @@ package com.EnusA.Prac4_1;
 public class App 
 {
     //AE
-
+    public static void main (String []args)
+    {
+        String stud1;
+        Student s = new Student("John", "Egbert", 16, "jegbert@mail.com", "2369874512", "Film");
+        System.out.println(s.output());
+        Lecturer l = new Lecturer("Jade", "Harley", 26, "jharley@mymail.com", "369214", "Agriculture");
+        System.out.println(l.display());
+    }
 
 
 }
